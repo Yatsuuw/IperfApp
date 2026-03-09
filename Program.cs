796733145d0e244d80df83c3bb33497d367011cd
Yatsuu <pieters.lucas87@gmail.com>
2026-03-09 23:37:30 +0100

    Ajout d'une sécurité try/catch pour contrôler les erreurs au démarrage de l'application

--- Program.cs
+++ Program.cs
@@ -10,7 +10,22 @@ static class Program
   {
     // To customize application configuration such as set high DPI settings or default font,
     // see https://aka.ms/applicationconfiguration.
-    ApplicationConfiguration.Initialize();
-    Application.Run(new Form1());
+    {
+    try
+    {
+      ApplicationConfiguration.Initialize();
+      Application.Run(new Form1());
+    }
+    catch (Exception ex)
+    {
+      MessageBox.Show(
+        $"Erreur critique au démarrage :\n\n{ex.GetType().Name}\n{ex.Message}\n\n{ex.StackTrace}",
+        "Erreur",
+        MessageBoxButtons.OK,
+        MessageBoxIcon.Error
+      );
+      Environment.Exit(1);
+    }
+  }
   }    
 }
