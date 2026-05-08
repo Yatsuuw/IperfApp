c2250977adec683da2d9aa721c405d94e1685711
Yatsuu <121401515+Yatsuuw@users.noreply.github.com>
2026-05-08 02:54:40 +0200

    refactor(build): centraliser System.Globalization dans GlobalUsings + TreatWarningsAsErrors
    - GlobalUsings.cs : ajout de 'global using System.Globalization' pour
      éviter les régressions CS0103 futures (CultureInfo utilisé dans
      CsvExporter et IperfEngine).
    
    - IperfApp.csproj : activation de <TreatWarningsAsErrors>true</TreatWarningsAsErrors>
      pour que les warnings CS4014 / CS8600 / CS8602 bloquent la compilation
      plutôt que de passer silencieusement au publish.

--- GlobalUsings.cs
+++ GlobalUsings.cs
@@ -3,7 +3,10 @@
 // System.Threading et System.Threading.Tasks sont déjà injectés automatiquement.
 // System.Text est conservé explicitement car ImplicitUsings ne l'injecte PAS
 // pour les projets de type WinForms (contrairement aux projets Console/Web).
+// System.Globalization est centralisé ici pour éviter les régressions CS0103
+// sur CultureInfo (utilisé dans CsvExporter et IperfEngine).
 global using System.Diagnostics;
+global using System.Globalization;
 global using System.IO;
 global using System.Text;
 global using System.Windows.Forms;
